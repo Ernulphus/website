2a731cedcb248829b6b5a0eb613b56cce41ae5e0
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2023-03-13 15:42:27 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -7,17 +7,50 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Boaz Kaufman's Personal Website
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+        <i>Under Construction</i>
+        </p>
       </header>
+      
+      <h2>Quick Links</h2>
+      <a
+        className="App-link"
+        href="https://github.com/Ernulphus"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub Profile
+      </a><br/>
+      <a
+        className="App-link"
+        href="https://www.linkedin.com/in/boaz-kaufman-4281b0174/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        LinkedIn Profile
+      </a>
+
+      <h2>Projects</h2>
+      <p>CS Capstone: AI Instrument Classification 
+      <a
+        className="App-link"
+        href="https://github.com/Ernulphus/capstone-Bsharp-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        React Native App
+      </a></p>
+      <br/>
+      <a
+        className="App-link"
+        href="https://eportfolios.macaulay.cuny.edu/kaufmanspringboard/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Undergraduate Thesis on AI
+      </a>
     </div>
   );
 }
