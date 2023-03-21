8d12b0646839dbde382e0ca1ee76cdfd2f3d8f39
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2023-03-21 07:44:39 -0400

    Make links from constant array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {links} from './constants.js';
 
 function App() {
+  const personalLinks = links.map((link) => (
+    <div><a
+        className="App-link"
+        href={link.href}
+        target="_blank"
+        rel="noopener noreferrer"
+      >{link.text}</a>
+      <br/></div>
+  ));
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Boaz Kaufman's Personal Website
         </p>
@@ -15,42 +24,9 @@ function App() {
       </header>
       
       <h2>Quick Links</h2>
-      <a
-        className="App-link"
-        href="https://github.com/Ernulphus"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        GitHub Profile
-      </a><br/>
-      <a
-        className="App-link"
-        href="https://www.linkedin.com/in/boaz-kaufman-4281b0174/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        LinkedIn Profile
-      </a>
-
-      <h2>Projects</h2>
-      <p>CS Capstone: AI Instrument Classification 
-      <a
-        className="App-link"
-        href="https://github.com/Ernulphus/capstone-Bsharp-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        React Native App
-      </a></p>
+      
       <br/>
-      <a
-        className="App-link"
-        href="https://eportfolios.macaulay.cuny.edu/kaufmanspringboard/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Undergraduate Thesis on AI
-      </a>
+      {personalLinks}
     </div>
   );
 }
