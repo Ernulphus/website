6d351662a48822baa172aa5943e8cd677d154c1c
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2023-03-21 09:28:34 -0400

    start project descriptions and routes

---
+++ src/components/Projects/WhimsicalBrowser.js
@@ -0,0 +1,16 @@
+const WhimsicalBrowser = {
+    name: 'Whimsical Browser',
+    id: 'whimsy',
+    desc: 'A simple browser written in Python with Tkinter.',
+    body: `
+# Whimsical Browser
+Based on Web Browser Engineering by Pavel Panchekha & Chris Harrelson.
+    `,
+    tags: [
+        'Python',
+        'Tkinter',
+        'Browsers',
+    ],
+};
+
+export default {WhimsicalBrowser};
