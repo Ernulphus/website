a1a92f3d97be9beaf9e2fcdcec174e4361bd03dd
Boaz Kaufman <bk1nyc@gmail.com>
2025-02-20 11:35:39 -0500

    Write FileDownloader onClick

--- src/components/FileDownloader/FileDownloader.jsx
+++ src/components/FileDownloader/FileDownloader.jsx
@@ -1,9 +1,19 @@
 import { Link } from 'react-router-dom';
+import DownloadIcon from '@mui/icons-material/Download';
 import './FileDownloader.css';
 
 function FileDownloader () {
+  const onButtonClick = () => {
+    const pdfUrl = "";
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "KaufmanResume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   return (
-    <Link></Link>
+    <Link onClick={onButtonClick}>Resume <DownloadIcon /></Link>
   )
 };
 
