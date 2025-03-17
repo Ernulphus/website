acfd047fee92ef8c42c71dd813070d3cfa1bf34e
Boaz Kaufman <bk1nyc@gmail.com>
2025-03-17 16:37:27 -0400

    Changes for SPH routing for GH Pages

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/website" >
         <Sidebar />
         <Routes>
           <Route path="/" element={<Home />} />
