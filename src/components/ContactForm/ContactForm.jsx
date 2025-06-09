eeedfa51cb1584dfc7a895336622b87c9ad5d104
Boaz Kaufman <bk1nyc@gmail.com>
2025-06-08 20:34:15 -0500

    Require return email

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -18,16 +18,19 @@ function ContactForm() {
   
   return (
     <form className="contact-form" onSubmit={sendEmail}>
-      <h3>Contact me for a website, security hardening, or other services!</h3>
       <input type="hidden" name="contact_number" />
       <label>Name</label>
       <input type="text" name="from_name" />
-      <label>Email</label>
-      <input type="email" name="from_email" />
+      <label>Email*</label>
+      <input type="email" name="from_email" required />
       <label>Subject</label>
       <input type="text" name="subject" />
       <label>Message</label>
-      <textarea name="html_message" />
+      <textarea name="html_message">
+        Hi Boaz!
+        I'd like to schedule a call for...
+        I'm available...
+      </textarea> 
       <input type="submit" value="Send" />
       { sent && (<p>Message sent!</p>)}
     </form>
