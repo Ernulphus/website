39c0d06ff3c11133891d029235e197449cb2c712
Boaz Kaufman <bk1nyc@gmail.com>
2025-07-25 23:02:00 -0400

    Remove button to close sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,11 @@
 // Component for webpage sidebar with internal and external links
-import {useState} from 'react';
 import {pages, links, contacts, pdfs} from "../constants";
 import { Link } from 'react-router-dom';
-
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import MenuIcon from '@mui/icons-material/Menu';
 
 import './Sidebar.css';
 import FileDownloader from './FileDownloader/FileDownloader';
 
 function Sidebar() {
-    const [open, setOpen] = useState(true);
     const navPages = pages.map((page) => (
       <Link
           to={page.href}
@@ -56,31 +51,10 @@ function Sidebar() {
       <FileDownloader className="sidebar-link-div" text={pdf.text} url={pdf.href} filename={pdf.filename} />
     ));
 
-    const openSidebar = () => {
-      setOpen(true);
-    }
-    const closeSidebar = () => {
-      setOpen(false);
-    }
-
     return (
-      <div className={`sidebar-wrapper ${open ? "is-open" : ""}`} >
+      <div className="sidebar-wrapper is-open" >
         <div className="sidebar-inner">
           <div className="sidebar">
-            {(
-              open
-              ? <button
-                  className='sidebarButton'
-                  type="button" 
-                  onClick={closeSidebar}>
-                    <ArrowBackIcon />
-                </button>
-              : <button className="sidebarButton" 
-                type="button" 
-                onClick={openSidebar}>
-                  <MenuIcon />
-                </button>
-            )}
             <div className='sidebarSection'>
               <b>Navigation</b>
               {navPages}
